3535866958b1a5bab85369ceef8ccecd99167371
Pikachu <samiullahtaunsvi@gmail.com>
2024-12-13 02:49:10 +1100

    second commit

--- home page.php
+++ home page.php
@@ -33,7 +33,7 @@
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg" id="navbar">
         <div class="container">
-          <a class="navbar-brand" href="index.php" id="logo"><span>T</span>ravel<span>T</span>our</a>
+          <a class="navbar-brand" href="index.html" id="logo"><span>T</span>ravel<span>T</span>our</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
             <span><i class="fa-solid fa-bars"></i></span>
           </button>
